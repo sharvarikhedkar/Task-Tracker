6f4130942eb9b8d7e7a2428763f4a293940f902a
sharvarikhedkar <anujakhedkar96@gmail.com>
2021-02-17 12:20:03 -0800

    feat: Added delete task functionality to application

--- src/App.js
+++ src/App.js
@@ -28,11 +28,16 @@ const App = () => {
         reminder: false,
     },
     ])
-    
+
+  //Delete task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id!== id))
+  }
+
   return (
     <div className='container'>
       <Header/>
-      <Tasks tasks = {tasks}/>
+      {tasks.length > 0 ? <Tasks tasks = {tasks} onDelete = {deleteTask}/> : 'No tasks to show'}
     </div>
   )
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,11 +1,11 @@
 import Task from './Task'
 
-const Tasks = ({tasks}) => {
+const Tasks = ({tasks,onDelete}) => {
         //looping through tasks and outputting component, passing task in as a prop
         return (
         <>
             {tasks.map((task) => (
-                <Task key = {task.id} task = {task}/>
+                <Task key = {task.id} task = {task} onDelete = {onDelete}/>
                 
             ))}
             
