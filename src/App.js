869778c6f3a0788b4419030b644a4cca37092bc5
sharvarikhedkar <anujakhedkar96@gmail.com>
2021-02-17 10:43:45 -0800

    Change: Added global state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import React from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import {useState} from 'react'
 
 function App() {
+  const [tasks,setTasks] = useState([
+    {
+        id: 1,
+        text: 'Appointment',
+        day: 'Feb 5 at 2pm',
+        reminder: true,
+    
+    },
+        
+    {
+        id: 2,
+        text: 'Flight',
+        day: 'Feb 8 at 3pm',
+        reminder: true,
+    
+    },
+    
+    {
+        id: 3,
+        text: 'Final exam',
+        day: 'Feb 1 at 2pm',
+        reminder: false,
+    },
+    ])
   return (
     <div className='container'>
       <Header/>
-      <Tasks/>
+      <Tasks tasks = {tasks}/>
     </div>
   )
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,30 +1,8 @@
-const tasks = [
-{
-    id: 1,
-    text: 'Appointment',
-    day: 'Feb 5 at 2pm',
-    reminder: true,
 
-},
-    
-{
-    id: 2,
-    text: 'Flight',
-    day: 'Feb 8 at 3pm',
-    reminder: true,
 
-},
-
-{
-    id: 3,
-    text: 'Final exam',
-    day: 'Feb 1 at 2pm',
-    reminder: false,
-},
-]
-
-const Tasks = () => {
-    return (
+const Tasks = ({tasks}) => {
+        
+        return (
         <div>
             {tasks.map(task => (
                 <h3 key = {task.id}>{task.text}</h3>
