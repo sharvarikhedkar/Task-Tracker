9cbcaf6ecbc52fc4f285e8a61b4ac95f045e2b4c
sharvarikhedkar <anujakhedkar96@gmail.com>
2021-02-16 22:30:14 -0800

    feat: Added Button component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,19 @@
 import PropTypes from 'prop-types'
-
 import React from 'react'
+import Button from './Button'
+
 const Header = ({title}) => {
+
+    const onClick = () => {
+        console.log('click')
+    }
+
     return (
-        <header>
-            <h1>{title}</h1> 
-        </header>
+            <header class = 'header'>
+                <h1>{title}</h1> 
+                <Button color = 'green' text = 'Add' onClick = {onClick}/>
+                
+            </header>
     )
 }
 
