0d2ae681c64b6d38f20521329a89972824e71a3e
sharvarikhedkar <anujakhedkar96@gmail.com>
2021-02-17 17:08:16 -0800

    feat: Added toggle reminder functionality to application

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,11 +1,16 @@
 import Task from './Task'
 
-const Tasks = ({tasks,onDelete}) => {
+const Tasks = ({tasks,onDelete,onToggle}) => {
         //looping through tasks and outputting component, passing task in as a prop
         return (
         <>
             {tasks.map((task) => (
-                <Task key = {task.id} task = {task} onDelete = {onDelete}/>
+                <Task
+                key = {task.id} 
+                task = {task} 
+                onDelete = {onDelete}
+                onToggle = {onToggle}
+                />
                 
             ))}
             
