108ecb816766f481a4377645027d9d137eae62c3
sharvarikhedkar <anujakhedkar96@gmail.com>
2021-02-26 18:46:56 -0800

    feat: Added toggle to add and show tasks

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,16 +2,14 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import Button from './Button'
 
-const Header = ({title}) => {
+const Header = ({title, onAdd}) => {
 
-    const onClick = () => {
-        console.log('click')
-    }
+    
 
     return (
             <header className = 'header'>
                 <h1>{title}</h1> 
-                <Button color = 'green' text = 'Add' onClick = {onClick}/>
+                <Button color = 'green' text = 'Add' onClick = {onAdd}/>
                 
             </header>
     )
