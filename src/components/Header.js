60c501d36e6a055dfba36a110e7a6e91cf7e7d4f
sharvarikhedkar <anujakhedkar96@gmail.com>
2021-02-26 19:02:42 -0800

    feat: Added styling to the addTasks

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,17 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import Button from './Button'
 
-const Header = ({title, onAdd}) => {
+const Header = ({title, onAdd, showAdd}) => {
 
     
 
     return (
             <header className = 'header'>
                 <h1>{title}</h1> 
-                <Button color = 'green' text = 'Add' onClick = {onAdd}/>
+                <Button color = 'green' 
+                color = {showAdd ? 'red' : 'green'} 
+                text = {showAdd ? 'Close' : 'Add'} 
+                onClick = {onAdd}/>
                 
             </header>
     )
