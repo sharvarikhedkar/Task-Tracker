e9f1071baca0cb770f7b575a44a12b195148229f
sharvarikhedkar <anujakhedkar96@gmail.com>
2021-03-06 16:53:19 -0800

    Fix: Fixed page reload

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
     return (
@@ -6,7 +7,8 @@ const Footer = () => {
             <p>
                 copyright &copy; 2021
             </p>
-            <a href = "/about">About</a>
+            
+            <Link to = "/about">About</Link>
             
         </footer>
     )
